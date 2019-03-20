8308d0a3b7d25edd696720946e5b2848f887cec0
Erika Szabo <erika.szabo.geo@gmail.com>
2019-03-20 21:28:51 +0000

    learned variables

---
+++ Chapter1-starting/variables.py
@@ -0,0 +1,25 @@
+a = 8
+print( a )
+
+a = b = c = d = 8
+print( d )
+
+a, b , c = 1, 2, 3
+print( a, b, c )
+
+# Inititalize a variable with and integer value
+var = 8
+print( var )
+
+# Assign a float value to the variable
+var = 3.142
+print( var )
+
+# Assign a string value to the variable
+var = 'Python in easy steps'
+print( var )
+
+
+# Assign a boolean value to the variable
+var = True
+print( var )
